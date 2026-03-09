8eff2527a7ba67f07320a1b13e431b33557e5af0
ParkChanWoo <dksktlq5@gmail.com>
2026-03-09 10:53:57 +0900

    z

--- README.md
+++
@@ -1,2 +0,0 @@
-# CH2_TeamProject
-
--- Skill/Skill.cpp
+++
@@ -1,8 +0,0 @@
-#include "Skill.h"
-
-USkill::USkill(ACharacter* NewOwner, const std::string& NewName, int NewMpCost)
-{
-	Owner = NewOwner;
-	Name = NewName;
-	MpCost = NewMpCost;
-}
